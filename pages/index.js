6ba6907edd944f032c62d2f9bc0a32692f4d77ef
Attila Horvath <sweeptic@gmail.com>
2025-04-02 13:48:10 +0200

    013 Linking Between Pages

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/NextJS-is-great-Framework">NextJS is great Framework</Link>
+        </li>
+        <li>
+          <Link href="/news/Something-Else">Something Else</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
