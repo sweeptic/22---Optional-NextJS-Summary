1bc189095be092232504cd3c02e25ea84219c42d
Attila Horvath <sweeptic@gmail.com>
2025-04-02 13:27:26 +0200

    012 Extracting Dynamic Route Data

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,3 +1,12 @@
+import { useRouter } from 'next/router';
+
 export default function SingleNews() {
+  const router = useRouter();
+
+  console.log('router object', router.query.newsId);
+
+  // send a request to the backend API
+  // to fetch the news item here
+
   return <div>single news</div>;
 }
